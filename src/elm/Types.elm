b34c4164923f8e2c01be217e42c2d0c0f43f8c5a
ngmiller <nick@ngmiller.io>
2018-09-08 11:13:40 -0600

    basic proposal and action types

--- src/elm/Types.elm
+++ src/elm/Types.elm
@@ -1,5 +1,8 @@
 module Types exposing (..)
 
+import Contract.DSGroup exposing (Action)
+import Json.Decode exposing (Decoder, decodeString, dict, string)
+import Dict exposing (Dict)
 import Eth.Types exposing (..)
 
 
@@ -7,3 +10,36 @@ type alias EthNode =
     { http : HttpProvider
     , ws : WebsocketProvider
     }
+
+
+type alias Proposal =
+    { id : Int
+    , description : String
+    , action : Action
+    }
+
+
+type alias Descriptions =
+    Dict String String
+
+
+decodeDescriptions : Decoder Descriptions
+decodeDescriptions =
+    dict string
+
+
+buildProposals : Descriptions -> List Action -> List Proposal
+buildProposals descriptions =
+    List.indexedMap
+        (\index action ->
+            { action = action
+            , id = index
+            , description =
+                case (Dict.get (toString index) descriptions) of
+                    Nothing ->
+                        Debug.crash "missing proposal id from dict"
+
+                    Just description ->
+                        description
+            }
+        )
